9cd2a6c012a08c261529ba254ac3d4338eaf2aa9
Robert <robt15@hotmail.com>
2020-03-12 11:45:41 -0400

    first

---
+++ src/main/java/com/example/backendservice/model/Person.java
@@ -0,0 +1,30 @@
+package com.example.backendservice.model;
+
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Person {
+    private final UUID id;
+    private final String name;
+
+
+    public Person(@JsonProperty("id") UUID id, 
+    @JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public UUID getId() {
+        return this.id;
+    }
+
+
+    public String getName() {
+        return this.name;
+    }
+
+
+
+}
